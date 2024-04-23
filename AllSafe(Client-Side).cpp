a1877eec092fb4fe10d6b405da3e5b8bc9a7f9a2
Yousef Shaheen <115540431+YS2100344@users.noreply.github.com>
2024-04-23 22:41:25 +0200

    Add files via upload

---
+++ AllSafe(Client-Side).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <thread>
+#include <mutex>
+#include <cctype>
+#include <algorithm>
+#include <cctype>
+
+#define MAX_LEN 200
+#define NUM_COLORS 6
+
+using namespace std;
+
+// Global variables
+bool exit_flag = false;
+thread t_send, t_recv;
+int client_socket;
+string def_col = "\033[0m";  // Default terminal color
+string colors[] = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
+string username;  // Global storage for username
+const string globalKey = "tkhcyberkey";  // Encryption key for Vigenère cipher
---
+++ AllSafe(Server-Side).cpp
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#include <fstream>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <csignal>
+#include <cstring>
+
+#define MAX_LEN 200
+#define NUM_COLORS 6
+#define PORT 6676
+#define MAX_CLIENTS 200
+
+using namespace std;
+// Structure to hold client information
+struct Terminal {
+    int id;
+    string name;
+    thread th;
+    bool isActive;
+};
+
+Terminal clients[MAX_CLIENTS];
+int server_socket;
+mutex clients_mtx, cout_mtx; // Mutexes for synchronized output
+string colors[NUM_COLORS] = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
+string def_col = "\033[0m"; // Default color for terminal output
+const string globalKey = "tkhcyberkey"; // Global encryption key
