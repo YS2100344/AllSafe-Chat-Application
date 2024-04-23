d59305541db4c5f9e477a2accfc423d5b88c0202
Yousef Shaheen <115540431+YS2100344@users.noreply.github.com>
2024-04-23 22:43:25 +0200

    Update AllSafe(Server-Side).cpp

--- AllSafe(Server-Side).cpp
+++ AllSafe(Server-Side).cpp
@@ -38,3 +38,20 @@ mutex clients_mtx, cout_mtx; // Mutexes for synchronized output
 string colors[NUM_COLORS] = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
 string def_col = "\033[0m"; // Default color for terminal output
 const string globalKey = "tkhcyberkey"; // Global encryption key
+
+void handle_client(int client_socket);
+string color(int code);
+void shared_print(const string &str, bool endLine);
+void log_message(const string &message);
+void set_name(int client_id, string name);
+int broadcast_message(const string &message, int sender_id);
+void end_connection(int client_id);
+string vigenere_encrypt(const string &text, const string &key);
+string vigenere_decrypt(const string &text, const string &key);
+int create_server_socket(int port);
+void shutdown_server(int signum);
+void handle_signal(int signal);
+bool verify_credentials(const string &username, const string &password);
+void save_user_credentials(const string &username, const string &password);
+string trimAndLower(const string &str);
+
