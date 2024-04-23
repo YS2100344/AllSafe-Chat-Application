95cac60b2adb06cb5bff14bde09bde22b2b6e791
Yousef Shaheen <115540431+YS2100344@users.noreply.github.com>
2024-04-23 22:42:56 +0200

    Update AllSafe(Client-Side).cpp

--- AllSafe(Client-Side).cpp
+++ AllSafe(Client-Side).cpp
@@ -29,3 +29,11 @@ string def_col = "\033[0m";  // Default terminal color
 string colors[] = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
 string username;  // Global storage for username
 const string globalKey = "tkhcyberkey";  // Encryption key for Vigenère cipher
+// Function prototypes
+void catch_ctrl_c(int signal);
+string color(int code);
+void send_message(int client_socket);
+void recv_message(int client_socket);
+string vigenere_encrypt(const string& text, const string& globalKey);
+void save_credentials(const string& username, const string& password);
+string vigenere_decrypt(const string& encryptedText, const string& globalKey);
